9f3641fff8bda0df164c8daf8f4a6eaac59b008c
ssllaavv <ssllaavv@gmail.com>
2024-10-21 15:27:19 +0300

    - fix error message for username in register-page.html

--- PYTHON WEB BASICS/Django_projects/petstagram_2/petstagram_2/accounts/validators.py
+++ PYTHON WEB BASICS/Django_projects/petstagram_2/petstagram_2/accounts/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 def validate_name_isalpha(value):
     if not value.isalpha():
-        raise ValidationError('The name must contains only letters!')
+        raise ValidationError('Name must contains only letters!')
 
 
 def validate_name_is_longer_then_two_cars(value):
